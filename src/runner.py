56f075c52defeac34d2f137b3660307f8bf71a1d
Mr-Sunglasses <Mr-Sunglasses@users.noreply.github.com>
2022-09-20 09:15:38 +0000

    :art: Python code fromated with psf/black

--- src/runner.py
+++ src/runner.py
@@ -1,15 +1,17 @@
 import os
 
+
 def runner():
-    if os.path.isdir('src'):
+    if os.path.isdir("src"):
         try:
-            os.system('pip3 install -r requirements.txt')
-            os.chdir('src')
-            os.system('python3 main.py')
+            os.system("pip3 install -r requirements.txt")
+            os.chdir("src")
+            os.system("python3 main.py")
         except:
             print("ERROR")
     else:
-        print('No src folder found')
+        print("No src folder found")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     runner()
--- src/writer.py
+++ src/writer.py
@@ -7,8 +7,11 @@ def create_new_file_folder(appName: str):
     :param appName: Name of Your app that you want to create
     :return: Project file
     """
-    os.mkdir('src')
-    with open('/Users/anton/Desktop/Development/Python/fleet-create-app/templates/main.template', 'r') as f, open('src/main.py', 'w') as fw:
+    os.mkdir("src")
+    with open(
+        "/Users/anton/Desktop/Development/Python/fleet-create-app/templates/main.template",
+        "r",
+    ) as f, open("src/main.py", "w") as fw:
         for line in f:
 
             fw.write(line)
@@ -16,26 +19,41 @@ def create_new_file_folder(appName: str):
 
 def create_git_ignore():
 
-    with open('/Users/anton/Desktop/Development/Python/fleet-create-app/templates/gitignore.template', 'r') as f, open('.gitignore', 'w') as fw:
+    with open(
+        "/Users/anton/Desktop/Development/Python/fleet-create-app/templates/gitignore.template",
+        "r",
+    ) as f, open(".gitignore", "w") as fw:
         for line in f:
 
             fw.write(line)
+
 
 def create_requirements():
 
-    with open('/Users/anton/Desktop/Development/Python/fleet-create-app/templates/requirements.template', 'r') as f, open('requirements.txt', 'w') as fw:
+    with open(
+        "/Users/anton/Desktop/Development/Python/fleet-create-app/templates/requirements.template",
+        "r",
+    ) as f, open("requirements.txt", "w") as fw:
         for line in f:
 
             fw.write(line)
+
 
 def create_license():
-    with open('/Users/anton/Desktop/Development/Python/fleet-create-app/templates/LICENSE.template', 'r') as f, open('LICENSE', 'w') as fw:
+    with open(
+        "/Users/anton/Desktop/Development/Python/fleet-create-app/templates/LICENSE.template",
+        "r",
+    ) as f, open("LICENSE", "w") as fw:
         for line in f:
             fw.write(line)
 
+
 def create_readme(appName: str):
-    with open('/Users/anton/Desktop/Development/Python/fleet-create-app/templates/README.template', 'r') as f, open('README.md', 'w') as fw:
-        fw.write(f'# {appName}')
+    with open(
+        "/Users/anton/Desktop/Development/Python/fleet-create-app/templates/README.template",
+        "r",
+    ) as f, open("README.md", "w") as fw:
+        fw.write(f"# {appName}")
         fw.write("\n")
 
         for line in f:
@@ -44,11 +62,10 @@ def create_readme(appName: str):
 
 def project_creator(Project_name: str):
 
-    os.mkdir(f'{Project_name}')
-    os.chdir(f'{Project_name}')
+    os.mkdir(f"{Project_name}")
+    os.chdir(f"{Project_name}")
     create_new_file_folder(appName=Project_name)
     create_git_ignore()
     create_requirements()
     create_readme(appName=Project_name)
     create_license()
-
